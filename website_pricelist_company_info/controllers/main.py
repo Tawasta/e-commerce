a8344e541867b24bc72f8db6758d56ca1d4fe094
difbit <timo.kekalainen@hotmail.com>
2020-02-27 15:43:41 +0200

    [IMP] main renamed to portal

--- website_pricelist_company_info/controllers/main.py
+++
@@ -1,9 +0,0 @@
-from odoo.addons.website_portal.controllers.main import website_account
-
-
-class CustomerPortal(website_account):
-
-    def __init__(self):
-        super(CustomerPortal, self).__init__()
-
-        self.OPTIONAL_BILLING_FIELDS += ['property_product_pricelist']
